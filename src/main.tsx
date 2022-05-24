626c74dbc50c2a5f3eef766fef7b708b442ed724
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-05-24 23:02:47 +0530

    Sass Removed

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom/client"
 
 import App from "./App"
 
-import "./scss/index.scss"
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
