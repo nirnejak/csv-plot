e94888e8081acb8d541a21a8a7d8145e1bd86c0f
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-07-02 23:34:50 +0530

    Link Primitive Component Updated

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -3,6 +3,10 @@ import * as React from "react"
 import styled from "../../utils/stitches.config"
 
 type Props = {
+  href?: string
+  target?: string
+  className?: string
+  style?: React.CSSProperties
   children?: React.ReactNode
 }
 
@@ -10,8 +14,24 @@ const StyledLink = styled("a", {
   color: "white",
 })
 
-const Link: React.FC<Props> = ({ children, ...restProps }) => {
-  return <StyledLink {...restProps}>{children}</StyledLink>
+const Link: React.FC<Props> = ({
+  href,
+  target,
+  className,
+  style,
+  children,
+}) => {
+  return (
+    <StyledLink
+      href={href}
+      target={target}
+      className={className}
+      style={style}
+      rel="noreferrer"
+    >
+      {children}
+    </StyledLink>
+  )
 }
 
 export default Link
