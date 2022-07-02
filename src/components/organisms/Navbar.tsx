3a91ae468fd501690fdfc8cf773fba3ab4fe1e34
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-07-02 23:13:35 +0530

    Navbar: Github Link Added

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,15 +1,21 @@
 import * as React from "react"
 
 import styled from "../../utils/stitches.config"
+import Github from "../atoms/icons/Github"
 
 const StyledNavbar = styled("nav", {
-  backgroundColor: "$slate12",
   height: "60px",
+
+  paddingLeft: 20,
+  paddingRight: 20,
+
   display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-between",
   alignItems: "center",
 
   borderBottom: "0.5px solid $slate5",
+  backgroundColor: "$slate12",
+  color: "white",
 })
 
 const NavbarBrand = styled("h1", {
@@ -18,11 +24,24 @@ const NavbarBrand = styled("h1", {
   fontSize: "1.4rem",
   color: "white",
 })
+const NavbarLinks = styled("div", {
+  color: "white",
+  width: 25,
+})
 
 const Navbar: React.FC = () => {
   return (
     <StyledNavbar>
       <NavbarBrand>CSV Plot</NavbarBrand>
+      <NavbarLinks>
+        <a
+          href="https://github.com/nirnejak/csv-plot"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Github />
+        </a>
+      </NavbarLinks>
     </StyledNavbar>
   )
 }
