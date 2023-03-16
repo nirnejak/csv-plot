ce6bd8c64b8a20318a6bb9b9071a631e669151c5
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-17 00:09:27 +0530

    useCSV: types fixed

--- src/hooks/useCSV.ts
+++ src/hooks/useCSV.ts
@@ -2,21 +2,25 @@ import * as React from "react"
 
 import Papa from "papaparse"
 
-const useCSV = (csv: string) => {
-  const [csvData, setCsvData] = React.useState<any[]>([])
+interface HookReturn {
+  csvData: Array<Record<string, string>>
+  error: any
+}
+
+const useCSV = (csv: string): HookReturn => {
+  const [csvData, setCsvData] = React.useState<Array<Record<string, string>>>(
+    []
+  )
   const [error, setError] = React.useState<string>("")
 
   React.useEffect(() => {
     if (csv) {
-      const parseCsv = async () => {
-        const parsedCsv = await Papa.parse(csv)
-        if (parsedCsv.errors.length) {
-          setError(parsedCsv.errors[0].message)
-        } else {
-          setCsvData(parsedCsv.data)
-        }
+      const parsedCsv = Papa.parse(csv)
+      if (parsedCsv.errors.length) {
+        setError(parsedCsv.errors[0].message)
+      } else {
+        setCsvData(parsedCsv.data as Array<Record<string, string>>)
       }
-      parseCsv()
     }
   }, [csv])
 
