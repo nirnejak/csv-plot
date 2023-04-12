579001153edfed6d3ae6c1fa2e3c47ba553e52e1
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-13 00:26:21 +0530

    Select Component: color fixes, value controls

--- src/components/atoms/Select.tsx
+++ src/components/atoms/Select.tsx
@@ -17,7 +17,6 @@ import {
   ScrollDownButton,
 } from "@radix-ui/react-select"
 import { Check, ChevronDown, ChevronUp } from "akar-icons"
-import classNames from "src/utils/classNames"
 
 interface ITEM_TYPE {
   label: string
@@ -31,6 +30,8 @@ interface GROUP_TYPE {
 }
 
 interface Props {
+  value: string
+  onValueChange: (value: string) => void
   options: GROUP_TYPE[] | ITEM_TYPE[]
   label: string
   placeholder: string
@@ -38,41 +39,40 @@ interface Props {
 }
 
 const Select: React.FC<Props> = ({
+  value,
+  onValueChange,
   label,
   placeholder,
   options,
   isGrouped = false,
 }) => (
-  <Root>
+  <Root value={value} onValueChange={onValueChange}>
     <Trigger
-      className={classNames(
-        "inline-flex items-center justify-between gap-2 rounded bg-neutral-800 text-sm leading-none text-neutral-300 outline-none hover:bg-neutral-700 data-[placeholder]:text-neutral-300",
-        "min-w-32 h-[35px] px-4"
-      )}
+      className="inline-flex h-[35px] w-full items-center justify-between gap-2 rounded bg-neutral-800 px-4 text-sm leading-none text-neutral-300 outline-none hover:bg-neutral-700 data-[placeholder]:text-neutral-300"
       aria-label={label}
     >
-      <Value placeholder={placeholder} />
+      <Value placeholder={placeholder} aria-label={value} />
       <Icon className="text-neutral-600">
         <ChevronDown size={12} />
       </Icon>
     </Trigger>
     <Portal>
       <Content className="overflow-hidden rounded bg-neutral-800">
-        <ScrollUpButton className="flex h-[25px] cursor-default items-center justify-center bg-neutral-800 text-neutral-300">
+        <ScrollUpButton className="flex h-6 cursor-default items-center justify-center text-neutral-600">
           <ChevronUp size={12} />
         </ScrollUpButton>
-        <Viewport className="px-2 py-3">
+        <Viewport className="p-2">
           {isGrouped
             ? (options as GROUP_TYPE[]).map((group, i) => (
                 <>
                   <Group>
-                    <Label className="px-4 text-xs leading-6 text-neutral-500">
+                    <Label className="px-3 text-xs leading-6 text-neutral-500">
                       {group.name}
                     </Label>
                     {group.items.map((item, index) => (
                       <Item
                         key={index}
-                        className="relative flex h-[25px] select-none items-center rounded pl-[25px] pr-[35px] text-sm leading-none text-neutral-400 data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-700 data-[disabled]:text-neutral-600 data-[highlighted]:text-neutral-300 data-[highlighted]:outline-none"
+                        className="relative flex h-[25px] select-none items-center rounded pl-6 pr-9 text-sm leading-none text-neutral-400 data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-700 data-[disabled]:text-neutral-600 data-[highlighted]:text-neutral-300 data-[highlighted]:outline-none"
                         value={item.value}
                         disabled={item.disabled}
                       >
@@ -91,7 +91,7 @@ const Select: React.FC<Props> = ({
             : (options as ITEM_TYPE[]).map((item, index) => (
                 <Item
                   key={index}
-                  className="relative flex h-[25px] select-none items-center rounded pl-[25px] pr-[35px] text-sm leading-none text-neutral-400 data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-700 data-[disabled]:text-neutral-600 data-[highlighted]:text-neutral-300 data-[highlighted]:outline-none"
+                  className="relative flex h-[25px] select-none items-center rounded pl-6 pr-9 text-sm leading-none text-neutral-400 data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-700 data-[disabled]:text-neutral-600 data-[highlighted]:text-neutral-300 data-[highlighted]:outline-none"
                   value={item.value}
                   disabled={item.disabled}
                 >
@@ -102,7 +102,7 @@ const Select: React.FC<Props> = ({
                 </Item>
               ))}
         </Viewport>
-        <ScrollDownButton className="flex h-[25px] cursor-default items-center justify-center bg-neutral-800 text-blue-600">
+        <ScrollDownButton className="flex h-6 cursor-default items-center justify-center text-neutral-600">
           <ChevronDown size={12} />
         </ScrollDownButton>
       </Content>
