e92447d136213f9525e132e6d1a9a2bd44734a90
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-31 12:13:26 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,19 @@
 import { fileURLToPath, URL } from "url"
 
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig, splitVendorChunkPlugin, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import svgrPlugin from "vite-plugin-svgr"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
-export default ({ mode }): any => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") }
-
-  return defineConfig({
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    // Object.assign(process.env, loadEnv(mode, process.cwd()));
-    // now you can access config with process.env.{configName}
-    build: {
-      target: "esnext",
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), svgrPlugin(), viteTsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    plugins: [
-      react(),
-      viteTsconfigPaths(),
-      svgrPlugin(),
-      splitVendorChunkPlugin(),
-    ],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-    server: {
-      port: 3000,
-    },
-  })
-}
+  },
+})
