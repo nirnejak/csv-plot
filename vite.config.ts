929a8c11de188fa8cce534d7d87c86e2cfdf80be
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-16 18:22:20 +0530

    Vite config and dependencies corrected

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { fileURLToPath, URL } from "url"
-
-import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 import svgrPlugin from "vite-plugin-svgr"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react(), svgrPlugin(), viteTsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    svgrPlugin(),
+    viteTsconfigPaths(),
+  ],
 })
